4267c002d152c69fe4eeb851af635f940b6a51c5
kvmvkxnt <kamron1007dada@gmail.com>
2022-01-20 17:39:07 +0500

    To-Do list is fully done))

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@ var elList = document.querySelector('.todo__list');
 var clearButton = document.querySelector('.todo__clear');
 var tasksLeft = document.querySelector('.todo__left');
 var taskFilter = document.querySelector('.todo__filter');
+var allTasksFilter = document.querySelector('#todo_all');
+var activeTasksFilter = document.querySelector('#todo_active');
+var completedTasksFilter = document.querySelector('#todo_completed');
 var checked = 0;
-var unchecked = 0;
-var uncompletedTasks = [];
 
 elForm.addEventListener('submit', handleFormSubmitter);
+clearButton.addEventListener('click', handleClear);
+allTasksFilter.addEventListener('click', showAllTasks);
+activeTasksFilter.addEventListener('click', showActiveTasks);
+completedTasksFilter.addEventListener('click', showCompletedTasks);
+allTasksFilter.style.borderColor = 'rgba(175, 47, 47, 0.2)';
 
 function handleFormSubmitter(evt) {
     evt.preventDefault();
@@ -21,53 +27,170 @@ function createNewElement() {
     if (newTask.length <= 0) {
         null
     }
-    else { // creating new element
+    else {
         var newItem = document.createElement('li');
-        newItem.className = 'todo__item';
+        var newLabel = document.createElement('label');
+        var newCheckbox = document.createElement('input');
+        var newCheck = document.createElement('span');
+        var newTaskDesc = document.createElement('p');
+        var newButton = document.createElement('button');
+        var newImg = document.createElement('img');
+
+        newItem.setAttribute('class', 'todo__item');
         newItem.setAttribute("onmouseover", "this.querySelector('.todo__button').style.visibility = 'visible'");
         newItem.setAttribute("onmouseout", "this.querySelector('.todo__button').style.visibility = 'hidden'");
-        newItem.innerHTML = `<label class="todo__label"><input type="checkbox" class="todo__checkbox visually-hidden"><span class="todo__check"></span><p class="todo__task">${newTask}</p></label><button class="todo__button"><img src="../images/x.svg" alt="Check"></button>`;
+        newLabel.setAttribute('class', 'todo__label');
+        newCheckbox.setAttribute('class', 'todo__checkbox visually-hidden');
+        newCheckbox.setAttribute('type', 'checkbox');
+        newCheck.setAttribute('class', 'todo__check');
+        newTaskDesc.setAttribute('class', 'todo__task');
+        newButton.setAttribute('class', 'todo__button');
+        newImg.setAttribute('src', '../images/x.svg');
+        newImg.setAttribute('alt', 'Check');
+
+        newTaskDesc.textContent = newTask;
+
+        newLabel.appendChild(newCheckbox);
+        newLabel.appendChild(newCheck);
+        newLabel.appendChild(newTaskDesc);
+        newButton.appendChild(newImg);
+        newItem.appendChild(newLabel)
+        newItem.appendChild(newButton);
         elList.appendChild(newItem);
 
-        unchecked += 1;
-    }
+        filterFinder(newItem);
 
-    function labelFunc(elem) {
-        var labelTask = elem.parentElement;
-        labelTask.classList.toggle('checked');
-        var tasks = document.querySelectorAll('.todo__item');
+        newButton.addEventListener('click', function() {
+            newItem.remove();
+            toDoLeft();
+            clearDisplay();
+        });
 
-        for (var z = 0; z < tasks.length; i++) {
-            var taskClass = tasks[z].className.split(' ');
-            checked = 0;
-
-            if (taskClass.includes('checked')) {
+        newCheckbox.addEventListener('change', function() {
+            if (this.checked) {
+                var checksItem = this.parentElement.parentElement;
+                this.classList.add('checked');
                 checked += 1;
-                unchecked -= 1;
+                toDoLeft();
+                clearDisplay();
+                var listClasses = elList.className.split(' ');
+                if (listClasses.includes('only-active')) {
+                    checksItem.style.display = 'none';
+                } else if (listClasses.includes('only-completed')) {
+                    checksItem.style.display = 'block';
+                } else {
+                    checksItem.style.display = 'block';
+                }
+                
+            } else {
+                var checksItem = this.parentElement.parentElement;
+                this.classList.remove('checked');
+                checked -= 1;
+                toDoLeft();
+                clearDisplay();
+                filterFinder(checksItem);
             }
-        }
+        });
     }
 
-    // Need to remake labelFunctio - problem: gettiing self and labels under twice
+    filterDisplay();
+    toDoLeft();
+    clearDisplay();
+}
 
-    var tasks = document.querySelectorAll('.todo__item');
-    if (tasks.length > 0) {
+function toDoLeft() {
+    var itemsLeft = document.querySelectorAll('.todo__checkbox').length - document.querySelectorAll('.checked').length;
+    tasksLeft.textContent = itemsLeft + ' items left';
+}
+
+function filterDisplay() {
+    var allItems = document.querySelectorAll('.todo__item');
+    if (allItems.length > 0) {
         taskFilter.style.display = 'flex';
     } else {
         taskFilter.style.display = 'none';
     }
+}
 
-    var elDeleteTask = document.querySelectorAll('.todo__button');
-    for (var i = 0; i < elDeleteTask.length; i++) {
-        elDeleteTask[i].addEventListener('click', function() {
-            this.parentElement.remove();
+function clearDisplay() {
+    var allChecked = document.querySelectorAll('.checked');
 
-            var tasks = document.querySelectorAll('.todo__item');
-            if (tasks.length > 0) {
-                taskFilter.style.display = 'flex';
-            } else {
-                taskFilter.style.display = 'none';
-            }
-        })
+    if (allChecked.length > 0) {
+        clearButton.style.visibility = 'visible';
+    } else {
+        clearButton.style.visibility = 'hidden';
     }
 }
+
+function handleClear() {
+    var allChecked = document.querySelectorAll('.checked');
+
+    for (var i = 0; i < allChecked.length; i++) {
+        var checkedTask = allChecked[i].parentElement.parentElement;
+        checkedTask.remove();
+    }
+
+    clearDisplay();
+}
+
+function showAllTasks() {
+    allTasksFilter.style.borderColor = 'rgba(175, 47, 47, 0.2)';
+    activeTasksFilter.style.borderColor = 'transparent';
+    completedTasksFilter.style.borderColor = 'transparent';
+    elList.className = 'todo__list';
+
+    var allTasks = document.querySelectorAll('.todo__item');
+
+    for (var i = 0; i < allTasks.length; i++) {
+        allTasks[i].style.display = 'block';
+    }
+}
+
+function showActiveTasks() {
+    activeTasksFilter.style.borderColor = 'rgba(175, 47, 47, 0.2)';
+    allTasksFilter.style.borderColor = 'transparent';
+    completedTasksFilter.style.borderColor = 'transparent';
+    elList.className = 'todo__list only-active';
+
+    var allTasks = document.querySelectorAll('.todo__item');
+    var completedChecks = document.querySelectorAll('.checked');
+
+    for (var i = 0; i < allTasks.length; i++) {
+        allTasks[i].style.display = 'block';
+    }
+
+    for (var z = 0; z < completedChecks.length; z++) {
+        var completedTask = completedChecks[z].parentElement.parentElement;
+        completedTask.style.display = 'none';
+    }
+}
+
+function showCompletedTasks() {
+    completedTasksFilter.style.borderColor = 'rgba(175, 47, 47, 0.2)';
+    activeTasksFilter.style.borderColor = 'transparent';
+    allTasksFilter.style.borderColor = 'transparent';
+    elList.className = 'todo__list only-completed';
+
+    var allTasks = document.querySelectorAll('.todo__item');
+    var completedChecks = document.querySelectorAll('.checked');
+
+    for (var i = 0; i < allTasks.length; i++) {
+        allTasks[i].style.display = 'none';
+    }
+
+    for (var z = 0; z < completedChecks.length; z++) {
+        var completedTask = completedChecks[z].parentElement.parentElement;
+        completedTask.style.display = 'block';
+    }
+}
+
+function filterFinder(checkItem) {
+    var listClasses = elList.className.split(' ');
+    if (listClasses.includes('only-active')) {
+        checkItem.style.display = 'block';
+    } else if (listClasses.includes('only-completed')) {
+        checkItem.style.display = 'none';
+    } else {
+        checkItem.style.display = 'block';
+    }
+} 
